2d01cc39c1f334621d58e37217c8dc136dd431c9
nevroje <joakim.berglind@gmail.com>
2019-04-02 14:27:22 +0200

    added api endpoint, setup get request in app.js got response pong

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import axios from "axios";
 class App extends Component {
+  async getKimai() {
+    let apiUrl = ""
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+    if (process.env.NODE_ENV === "development") {
+      debugger;
+      apiUrl = proxyUrl + "http://demo.kimai.org/api/";
+      console.log(process.env.NODE_ENV);
+    } else {
+      apiUrl = "https://demo.kimai.org/api/";
+      console.log(process.env.NODE_ENV);
+    }
+    let headers = {
+      "X-AUTH-USER": "susan_super",
+      "X-AUTH-TOKEN": "api_kitten"
+    };
+    try {
+      const response = await axios.get(apiUrl + "ping", {
+        headers: headers
+      });
+      const kimai = await response;
+      console.log(kimai);
+      return kimai;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
     return (
       <>
+        <p>hello</p>
+        <button onClick={() => this.getKimai()}>ping</button>
       </>
-    )
+    );
   }
 }
 
