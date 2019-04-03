9428ab715e0df92240e474279150681836ccc933
olivia <olivia@evolve.se>
2019-04-03 11:43:38 +0200

    add test for logo and tab1, add styling for sidebar

--- src/App.js
+++ src/App.js
@@ -23,36 +23,38 @@ class App extends Component {
 
 		return (
 			<Grid columns={2}>
-				<Grid.Column>
-					<Button disabled={visible} onClick={this.handleShowClick}>
-						<IoIosMenu />
+				<Grid.Column style={{background: "#5bb695", width: "90px", height: "1200px"}}>
+					<Button disabled={visible} onClick={this.handleShowClick} style={{background: "#5bb695"}}>
+						<IoIosMenu size={30}/>
 					</Button>
 				</Grid.Column>
-					<Sidebar
-						as={Menu}
-						// animation='overlay'
-						// icon='labeled'
-						inverted
-						onHide={() => {
-							this.handleSidebarHide()
-							this.updateLog('onHide')()
-						}}
-						onShow={this.updateLog('onShow')}
-						onVisible={this.updateLog('onVisible')}
-						vertical
-						visible={visible}
-						style={{
-							background:'#46b395',
-							width:'300px'
-						}}
-					>
-					  <img src={logo} alt="logo" style={{paddingTop: '20px', marginBottom: '25px', width: '207px', height: '135px'}} />
-						<Menu.Item as='a' style={{background: '#dbdbdf', marginBottom: '10px'}}>Tab1</Menu.Item>
-						<Menu.Item as='a' style={{background: '#dbdbdf', marginBottom: '10px'}}>Tab2</Menu.Item>
-						<Menu.Item as='a' style={{background: '#dbdbdf', marginBottom: '10px'}}>Tab3</Menu.Item>
-						<Menu.Item as='a' style={{background: '#dbdbdf', marginBottom: '10px'}}>Tab4</Menu.Item>
-						<Menu.Item as='a' style={{background: '#dbdbdf', marginBottom: '10px'}}>Tab5</Menu.Item>
-					</Sidebar>
+				<Sidebar
+					as={Menu}
+					// animation='overlay'
+					// icon='labeled'
+					inverted
+					onHide={() => {
+						this.handleSidebarHide()
+						this.updateLog('onHide')()
+					}}
+					onShow={this.updateLog('onShow')}
+					onVisible={this.updateLog('onVisible')}
+					vertical
+					visible={visible}
+					style={{
+						background: '#46b395',
+						width: '300px'
+					}}
+				>
+					<div className="form">
+						<img src={logo} alt="logo" style={{ paddingTop: '20px', marginBottom: '25px', width: '207px', height: '135px' }} />
+					</div>
+					<Menu.Item className="item" as='a' style={{ background: '#dbdbdf', marginBottom: '10px' }}>Tab1</Menu.Item>
+					<Menu.Item as='a' style={{ background: '#dbdbdf', marginBottom: '10px' }}>Tab2</Menu.Item>
+					<Menu.Item as='a' style={{ background: '#dbdbdf', marginBottom: '10px' }}>Tab3</Menu.Item>
+					<Menu.Item as='a' style={{ background: '#dbdbdf', marginBottom: '10px' }}>Tab4</Menu.Item>
+					<Menu.Item as='a' style={{ background: '#dbdbdf', marginBottom: '10px' }}>Tab5</Menu.Item>
+				</Sidebar>
 			</Grid>
 		)
 	}
