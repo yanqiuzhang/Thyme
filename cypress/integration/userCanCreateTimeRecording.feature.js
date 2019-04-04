10ce477d39dbdd5fae9c98267261c8a53e4f425a
nici <niciputter@yahoo.com>
2019-04-04 17:33:20 +0200

    config successful path api test and add fixtures object

--- cypress/integration/userCanCreateTimeRecording.feature.js
+++ cypress/integration/userCanCreateTimeRecording.feature.js
@@ -1,10 +1,16 @@
 describe("User can create time recording", () => {
-	it("when user visits the page", () => {
+	it("successfully", () => {
+		cy.server();
+		cy.route({
+			method: 'POST',
+			url: 'https://demo-stable.kimai.org/api/timesheets',
+			response: 'fixtures:save_data.json',
+			headers: {
+				"X-AUTH-USER": "susan_super",
+				"X-AUTH-TOKEN": "api_kitten"
+			}
+		})
 		cy.visit("http://localhost:3000");
-		cy.get('input[name="begin"]');
-		cy.get('label[name="Start Time"]').should("contain", "Start Time");
-		cy.get('input[name="end"]');
-		cy.get('label[name="End Time"]').should("contain", "End Time");
 		cy.get('input[name="begin"]').type("07:00");
 		cy.get('input[name="end"]').type("08:00");
 		cy.get('button[name="create"]').click();
